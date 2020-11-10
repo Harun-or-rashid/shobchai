655ea8c61e50c2f9fe313d5ef54ba74aa3b90b15
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-11-10 15:24:50 +0600

    PicEdit library added and implemented in gallery

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -23,7 +23,7 @@ class GalleryController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.admin.galleries.create');
     }
 
     /**
--- resources/views/backend/admin/galleries/create.blade.php
+++ resources/views/backend/admin/galleries/create.blade.php
@@ -1,7 +1,9 @@
 @extends('backend.partial.app')
-@section('title','Category')
+@section('title','Gallery')
 @section('main_section')
 <div class="row">
+    <link rel="stylesheet" type="text/css" href="{{asset('picEdit/dist')}}/css/picedit.min.css" />
+
     <div class="col-md-8">
         <div class="card">
             <div class="card-body">
@@ -9,18 +11,21 @@
                 <form class="form-group category-create" id="category-create" name="category-create"  action="{{route('category.store')}}" method="post">
                   @include('backend.partial.session_messages')
                     @csrf
+
+                    <div  class="col-md-8 mt-3 form-group picedit_box" style="height: 100%;width: 100%;">
+                        <label class="col-form-label" for="category-name">Upload & Edit Image</label>
+                        <input  type="file" name="image" id="image" class="form-control-file">
+                    </div>
                     <div class="col-md-5">
-                        <label class="col-form-label" for="category-name">Category Name</label>
-                        <input class="form-control" type="text" name="category" id="category">
-                            <span class="text-danger">{{ $errors->first('category') }}</span>
+                        <label class="col-form-label" for="category-name">Image Title</label>
+                        <input class="form-control" type="text" name="title" id="title">
+                        <span class="text-danger">{{ $errors->first('category') }}</span>
                     </div>
                     <div class="col-md-5">
                         <label class="col-form-label" for="parent-name">Select Parent</label>
                         <select class="form-control" name="parent" id="">
                             <option value="">Select Any..</option>
-                            @foreach($categories as $data)
-                                <option value="{{$data->id}}">{{$data->category_name}}</option>
-                            @endforeach
+
                         </select>
                     </div>
 
@@ -35,10 +40,15 @@
 
 
 
-
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="{{asset('picEdit/dist')}}/js/picedit.min.js"></script>
 
 <script>
-
+    $('#image').picEdit({
+        formSubmitted: function(response){
+            alert('Form submitted!');
+        }
+    });
         function validateFm(){
             $(".category-create").validate({
                 rules: {
@@ -54,6 +64,9 @@
             });
 
         }
+
+</script>
+<script type="text/javascript">
 
 </script>
 @endsection
