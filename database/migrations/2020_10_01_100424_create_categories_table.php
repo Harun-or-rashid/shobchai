da9cb77ba9d6fb8e79dfffac54c3830232d568eb
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-11-14 23:47:05 +0600

    sidebar changed and also migrations

--- database/migrations/2020_10_01_100424_create_categories_table.php
+++ database/migrations/2020_10_01_100424_create_categories_table.php
@@ -14,7 +14,8 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-
+            $table->increments('id');
+            $table->string('category_name');
             $table->timestamps();
         });
     }
--- resources/views/backend/partial/layouts/sidebar.blade.php
+++ resources/views/backend/partial/layouts/sidebar.blade.php
@@ -47,6 +47,25 @@
                                 <p>Add New</p>
                             </a>
                         </li>
+                        <li class="nav-item has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>
+                                    Sub Categories
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="far fa-dot-circle nav-icon"></i>
+                                        <p>Add More</p>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
+
 
                     </ul>
                 </li>
