8286c4689dfba7270a21db6e82c3e6fb7b7077a6
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-11-16 21:28:31 +0600

    blade files added for variance

---
+++ resources/views/backend/admin/variances/create.blade.php
@@ -0,0 +1,50 @@
+@extends('backend.partial.app')
+@section('title','Category')
+@section('main_section')
+<div class="row">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="form-control-navbar badge badge-info">Add New Category</h5>
+                <form class="form-group category-create" id="category-create" name="category-create"  action="{{route('category.store')}}" method="post">
+                  @include('backend.partial.session_messages')
+                    @csrf
+                    <div class="col-md-5">
+                        <label class="col-form-label" for="category-name">Category Name</label>
+                        <input class="form-control" type="text" name="category_name" id="category">
+                            <span class="text-danger">{{ $errors->first('category_name') }}</span>
+                    </div>
+
+                    <button class="btn btn-outline-info form-row mt-3" onclick="validateFm()" type="submit">Create</button>
+{{--                    <a class="btn btn-outline-info form-row mt-3" type="submit">Create</a>--}}
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<script>
+
+        function validateFm(){
+            $(".category-create").validate({
+                rules: {
+                    category: "required",
+                    // parent: "required",
+
+                },
+                messages:{
+                    category: "required",
+                    // parent: "required",
+
+                }
+            });
+
+        }
+
+</script>
+@endsection
